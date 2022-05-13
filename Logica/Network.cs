8e87685d48c28339c07c6fb23aa6854c18668456
Jéssica Trettin dos Santos <jessica.trettin@ambevtech.com.br>
2022-05-13 16:53:03 -0300

    Add project files.

---
+++ Logica/Network.cs
@@ -0,0 +1,87 @@
+﻿namespace TesteLogica
+{
+    public class Network
+    {
+        public int NumberOfElements { get; set; }
+
+        public Network()
+        {
+
+        }
+
+        public Array Set(int numberOfElements)
+        {
+            if (numberOfElements <= 0)
+            {
+                throw new Exception("Número inválido! É necessário inserir um número inteiro positivo.");
+            }
+
+            int[] setElements = new int[numberOfElements];
+            for (int i = 1; i <= numberOfElements; i++)
+            {
+                setElements[i] = i;
+            }
+            return setElements;
+        }
+
+
+        public void NumbersExistInSet(Array set, int num)
+        {
+            var verification = true;
+
+            if (verification)
+            {
+                foreach (int element in set)
+                {
+                    while (num != element)
+                    {
+                        verification = true;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Número não existe na sequência");
+            }
+        }
+
+        public Array Connect(Array set, int n1, int n2)
+        {
+            NumbersExistInSet(set, n1);
+            NumbersExistInSet(set, n2);
+
+            int[,] connection = new int[n1, n2];
+            return connection;
+        }
+
+        public List<Array> Connections(Array connection)
+        {
+            List<Array> connections = new List<Array>();
+            connections.Add(connection);
+            return connections;
+        }
+
+        public bool Query(List<Array> connections, int n1, int n2)
+        {
+            int[,] connectionVerify = new int[n1, n2];
+            var verification = 0;
+
+            foreach (var connection in connections)
+            {
+                if (connection == connectionVerify)
+                {
+                    verification += 1;
+                }
+            }
+
+            if (verification != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Logica/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using TesteLogica;
+
+public class Program : Network
+{
+    private readonly Network _network;
+
+    public Program(Network network)
+    {
+        _network = network;
+    }
+
+    static void Main()
+    {
+        Console.Write("");
+    }
+    public void Run()
+    {
+        Console.Write("Digite o número de elementos da série: ");
+        int numberOfElements = Convert.ToInt32(Console.ReadLine());
+
+        var set = _network.Set(numberOfElements);
+
+        Console.WriteLine("");
+        Console.WriteLine("Agora vamos conectar os números");
+
+        var connectionList = new List<Array> ();
+        string resp = "S";
+        while (resp == "S")
+        {
+            Console.Write("Número 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            var conection = _network.Connect(set, num1, num2);
+            connectionList = _network.Connections(conection);
+
+            Console.Write("Deseja conectar mais 2 números? (S/N): ");
+            resp = Console.ReadLine().ToUpper();
+
+        }
+        Console.WriteLine("As conexões são:");
+        for (int i = 0; i < connectionList.Count; i++)
+        {
+            Console.WriteLine(connectionList[i]);
+        }
+
+        Console.WriteLine("Agora você pode verificar se 2 números estão conectados:");
+        string resp2 = "S";
+        while (resp2 == "S")
+        {
+            Console.Write("Número 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            var conectionIsTrue = _network.Query(connectionList, num1, num2);
+
+            if (conectionIsTrue == true)
+            {
+                Console.WriteLine("Os números estão conectados");
+            }
+            else
+            {
+                Console.WriteLine("Os números não estão conectados");
+            }
+            Console.WriteLine("");
+
+            Console.Write("Deseja verificar mais 2 números? (S/N): ");
+            resp2 = Console.ReadLine().ToUpper();
+        }
+        Console.WriteLine("FIM");
+    }
+}
